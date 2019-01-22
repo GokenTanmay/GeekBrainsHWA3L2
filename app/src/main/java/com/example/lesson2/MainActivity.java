9309e2b5cbe599d8c409be42c3ee6bbcfdba7f53
GknTnm <gkntnm@gmail.com>
2019-01-23 03:04:08 +0400

    Initial

---
+++ app/src/main/java/com/example/lesson2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.lesson2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextField;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextField = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView2);
+
+        editTextField.addTextChangedListener(textWatcher);
+    }
+
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            textView.setText(s);
+        }
+    };
+}
+
